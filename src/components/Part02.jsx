d44c7155d83fc2c439cda2b1c61235b6ef224e99
monisato <monicasakane@gmail.com>
2021-03-24 18:29:41 -0300

    page 04 terminado

--- src/components/Part02.jsx
+++ src/components/Part02.jsx
@@ -4,19 +4,15 @@ import '../styles/Part02.css';
 class Part02 extends Component {
     render() { 
         return (
-            <div className="PartPage" idName="part02">
+            <div className="PartPage">
                 <div className="FullImg">
                     <img className="Fundo" src="./imgs/pg02_notxt_background.png" alt="mas um microondas diferente, ele tem um buraco!"/>
-                    <div className="MarContainerP2">
-                        <img className="MarOlhoP2" src="./imgs/pg02_notxt_mar_olho.png" alt="mas um microondas diferente, ele tem um buraco!"/>
-                    </div>
-                    <div className="MicPg2Parts">
-                        <img className="Balao" src="./imgs/pg02_notxt_balao.png" alt="mas um microondas diferente, ele tem um buraco!"/>
-                        <img className="MicOlhoP2" src="./imgs/pg02_notxt_mic_olho.png" alt="mas um microondas diferente, ele tem um buraco!"/>
-                        <img className="Buraco" src="./imgs/pg02_notxt_buraco.png" alt="mas um microondas diferente, ele tem um buraco!"/>
-                    </div>
-                    <p className="TxtPage02 Linha1" >mas um microondas &#013;diferente</p>
-                    <p className="TxtPage02 Linha2" >ele tem um buraco!</p>
+                    <img className="MarOlhoP2" src="./imgs/pg02_notxt_mar_olho.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                    <img className="Balao" src="./imgs/pg02_notxt_balao.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                    <img className="MicOlhoP2" src="./imgs/pg02_notxt_mic_olho.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                    <img className="Buraco" src="./imgs/pg02_notxt_buraco.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                    <p className="TxtPage02">mas um microondas &#013;diferente</p>
+                    <p className="TxtPage02 Linha2">ele tem um buraco!</p>
                 </div>
             </div>
         );
--- src/components/Part03.jsx
+++ src/components/Part03.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import '../styles/Part03.css';
 
 class Part03 extends Component {
     render() { 
         return (
-            <div className="PartPage" idName="part03">
-                <img className="Fundo" src="./imgs/pg03.jpeg" alt="sua mãe e amigos falavam: -Essa coisa está quebrada!"/>
+            <div className="PartPage">
+                <div className="FullImg">
+                    <img className="Fundo" src="./imgs/pg03_notxt_background.png" alt="sua mãe e amigos falavam: -Essa coisa está quebrada!"/>
+                    <img className="MaeBoca" src="./imgs/pg03_notxt_mae_boca.png" alt="sua mãe e amigos falavam: -Essa coisa está quebrada!"/>
+                    <img className="MaeOlho" src="./imgs/pg03_notxt_mae_olho.png" alt="sua mãe e amigos falavam: -Essa coisa está quebrada!"/>
+                    <img className="MicBoca" src="./imgs/pg03_notxt_mic_boca.png" alt="sua mãe e amigos falavam: -Essa coisa está quebrada!"/>
+                    <img className="Cara1" src="./imgs/pg03_notxt_cara1.png" alt="sua mãe e amigos falavam: -Essa coisa está quebrada!"/>
+                    <img className="Cara2" src="./imgs/pg03_notxt_cara2.png" alt="sua mãe e amigos falavam: -Essa coisa está quebrada!"/>
+                    <p className="TxtPage03">sua mãe e amigos falavam: &#013;-Essa coisa está quebrada!</p>
+                </div>
             </div>
         );
     }
