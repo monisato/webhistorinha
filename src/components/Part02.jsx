2ca5ee87805b395287b6a3e558eb2d37e78243a8
monisato <monicasakane@gmail.com>
2021-03-24 15:50:08 -0300

    page 2 terminado

--- src/components/Part02.jsx
+++ src/components/Part02.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import '../styles/Part02.css';
 
 class Part02 extends Component {
     render() { 
         return (
             <div className="PartPage" idName="part02">
-                <img className="Fundo" src="./imgs/pg02.jpeg" alt="mas um microondas diferente, ele tem um buraco!"/>
+                <div className="FullImg">
+                    <img className="Fundo" src="./imgs/pg02_notxt_background.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                    <div className="MarContainerP2">
+                        <img className="MarOlhoP2" src="./imgs/pg02_notxt_mar_olho.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                    </div>
+                    <div className="MicPg2Parts">
+                        <img className="Balao" src="./imgs/pg02_notxt_balao.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                        <img className="MicOlhoP2" src="./imgs/pg02_notxt_mic_olho.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                        <img className="Buraco" src="./imgs/pg02_notxt_buraco.png" alt="mas um microondas diferente, ele tem um buraco!"/>
+                    </div>
+                    <p className="TxtPage02 Linha1" >mas um microondas &#013;diferente</p>
+                    <p className="TxtPage02 Linha2" >ele tem um buraco!</p>
+                </div>
             </div>
         );
     }
